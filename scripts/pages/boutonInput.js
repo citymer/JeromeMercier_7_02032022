b91b95d597a1b4cd4cefa1c7bc22a6e38c79c39b
citymer <cityhunter8484@gmail.com>
2022-03-17 15:54:31 +0100

    trie tableau ingredient,appareils,ustensiles sans doublon

--- scripts/pages/boutonInput.js
+++ scripts/pages/boutonInput.js
@@ -1,7 +1,46 @@
 // creation tableau
 
-let tableau = [];
+let ingredientArray = [];
+let appareilArray = [];
+let ustensileArray = [];
+
+
+// recupere tous les ingredients
+recipes.forEach((recettes) => {
+    recettes.ingredients.forEach((recette) => {
+        ingredientArray.push(recette.ingredient);
+    })
+})
+
+
+// recupere tous les appareils 
+recipes.forEach((recettes) => {
+    appareilArray.push(recettes.appliance);
+ 
+})
+
+// recupere tous les ustensiles
+recipes.forEach((recettes) => {
+    recettes.ustensils.forEach((recette) => {
+        ustensileArray.push(recette);
+    })
+ 
+})
 
 
 
-console.log(tableau);
+
+// supprime les doublons dans "ingredientArray" et crée un nouveau tableau
+const filteredIngredient = ingredientArray.filter((el, index) => ingredientArray.indexOf(el) !== index)
+const ingredientsFiltre = [...new Set(filteredIngredient)]
+console.log(ingredientsFiltre);
+
+// supprime les doublons dans "appareilArray" et crée un nouveau tableau
+const filteredAppareil = appareilArray.filter((el, index) => appareilArray.indexOf(el) !== index)
+const appareilFiltre = [...new Set(filteredAppareil)]
+console.log(appareilFiltre);
+
+// supprime les doublons dans "ustensileArray" et crée un nouveau tableau
+const filteredUstensile = ustensileArray.filter((el, index) => ustensileArray.indexOf(el) !== index)
+const ustensileFiltre = [...new Set(filteredUstensile)]
+console.log(ustensileFiltre);
